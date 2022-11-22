64481c417856e5ab2146e07441f262be2bf38a9d
jacob-seibert <jacob.h.seibert@gmail.com>
2022-11-22 22:41:35 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg';
+import Navigation from './Components/Navigation/Navigation.js';
+import Logo from './Components/Logo/Logo.js';
+import Rank from './Components/Rank/Rank.js';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg className="particles" color="#FFFFFF" num={350} type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/*
+Code to implement ParticlesBg:
+<Particlesbg type='circle' bg={true} />
+
+Code to implement Tilt:
+import Tilt from 'react-parallax-tilt';
+------------------------------------
+<tilt>
+....
+</tilt>
+*/
+
